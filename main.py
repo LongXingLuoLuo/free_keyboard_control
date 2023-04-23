c53aa0fa209426e1dbdfa5579e0c492d5a27a4e6
LongXingLuoLuo <2473475362@qq.com>
2023-04-24 01:19:08 +0800

    实现 main.py

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QLabel
+import sys
 
-from utils import screenShotByKey
+from PyQt5 import QtWidgets
+
+import auto_action
+from pyUI import MainWindow
 
 
-class MyWidget(QWidget):
-    """实现全透明窗口"""
-
-    def __init__(self):
-        super(MyWidget, self).__init__()
-
-        # 去除背景
-        self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
-        self.setAttribute(Qt.WA_TranslucentBackground)
-
-        # 窗口设置
-        self.resize(320, 240)
-        self.setWindowTitle('Test')
-        self.setObjectName('MyWidget')
-        self.setStyleSheet("QWidget#MyWidget{background:grey;}")
-
-        # 添加个标签图片
-        self.label = QLabel(self)
-        self.label.setGeometry(120, 80, 66, 66)
-        self.label.setText("(12, 28)")
+def test_main():
+    app = QtWidgets.QApplication(sys.argv)
+    win = MainWindow(actionGroupDict=auto_action.readAllJsons())
+    win.show()
+    sys.exit(app.exec())
 
 
 if __name__ == '__main__':
-    p = screenShotByKey()
-    print(p)
+    test_main()
