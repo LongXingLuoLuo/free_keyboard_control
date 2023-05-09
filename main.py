94ea32cbba03d9a4ee869e3258773f15943d82ce
LongXingLuoLuo <2473475362@qq.com>
2023-05-09 10:41:24 +0800

    修复界面字体太小的问题

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import sys
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
 
 from auto_action import readAllJsons
 from pyUI import MainWindow
 
 if __name__ == '__main__':
+    # ? enable dpi scale
+    QApplication.setHighDpiScaleFactorRoundingPolicy(
+        Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+
     app = QApplication(sys.argv)
     win = MainWindow(actionGroupDict=readAllJsons())
     win.show()
